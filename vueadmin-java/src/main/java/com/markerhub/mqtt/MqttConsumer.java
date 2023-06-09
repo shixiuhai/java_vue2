ab50775954fa3e0a2729261c8eff2e82b80c69d4
shixiuhai <15256728901@163.com>
2023-06-09 23:56:35 +0800

    mqtt实现了设备上线离线的监听

--- vueadmin-java/src/main/java/com/markerhub/mqtt/MqttConsumer.java
+++ vueadmin-java/src/main/java/com/markerhub/mqtt/MqttConsumer.java
@@ -12,25 +12,40 @@ public class MqttConsumer {
     @Value("${spring.mqtt.receiveTopics}")
     private String[] receiveTopics;
     public void consumerTopic(Message<?> message){
-        System.out.println(receiveTopics[1]);
+        // System.out.println(receiveTopics[1]);
         // System.out.println(message.getPayload());
         // System.out.println(message.getHeaders());
-        System.out.println(message.getHeaders().get("mqtt_receivedTopic"));
+        // System.out.println(message.getHeaders().get("mqtt_receivedTopic"));
         
-        if(message.getHeaders().get("mqtt_receivedTopic").equals(realTopic(receiveTopics[0]))){
-            System.out.println("fist----topic");
-            // System.out.println(message.getPayload());
-            JSONObject result = JSONObject.parseObject(message.getPayload().toString());
-            System.out.println(result);
+        // 设备上线
+        if(realTopic(message.getHeaders().get("mqtt_receivedTopic").toString()).equals(realTopic(receiveTopics[0]))){
+            System.out.println("fist----topic---contend");
+            System.out.println(message.getPayload());
+            // JSONObject result = JSONObject.parseObject(message.getPayload().toString());
+            // System.out.println(result);
 
         }
-        if(message.getHeaders().get("mqtt_receivedTopic").equals(realTopic(receiveTopics[1]))){
-            // System.out.println(message.getPayload());
-            JSONObject result = JSONObject.parseObject(message.getPayload().toString());
-            // System.out.println("second---topic");
-            System.out.println(result.getString("msg"));
+        // 设备下线
+        if(realTopic(message.getHeaders().get("mqtt_receivedTopic").toString()).equals(realTopic(receiveTopics[1]))){
+            System.out.println("second---topic---disconectd");
+            
+            System.out.println(message.getPayload());
+
+            // JSONObject result = JSONObject.parseObject(message.getPayload().toString());
+            // System.out.println(result.getString("msg"));
 
         }
+        // 火焰报警
+        if(realTopic(message.getHeaders().get("mqtt_receivedTopic").toString()).equals(realTopic(receiveTopics[2]))){
+            System.out.println("second---topic---disconectd");
+            
+            System.out.println(message.getPayload());
+
+            // JSONObject result = JSONObject.parseObject(message.getPayload().toString());
+            // System.out.println(result.getString("msg"));
+
+        }
+
             
        
         
