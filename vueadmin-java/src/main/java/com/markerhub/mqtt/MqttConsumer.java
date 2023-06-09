0eadf61adda9ac2431f19d5ae8ae7f1a190600d5
shixiuhai <15256728901@163.com>
2023-06-09 21:59:55 +0800

    更新了监听消息

--- vueadmin-java/src/main/java/com/markerhub/mqtt/MqttConsumer.java
+++ vueadmin-java/src/main/java/com/markerhub/mqtt/MqttConsumer.java
@@ -1,6 +1,9 @@
 package com.markerhub.mqtt;
 
 import org.springframework.stereotype.Service;
+
+import com.alibaba.fastjson.JSONObject;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.messaging.Message;
 
@@ -11,13 +14,23 @@ public class MqttConsumer {
     public void consumerTopic(Message<?> message){
         // System.out.println(receiveTopics[0]);
         // System.out.println(message.getPayload());
-        System.out.println(message.getHeaders().get("mqtt_receivedTopic"));
-            if(message.getHeaders().get("mqtt_receivedTopic").equals(receiveTopics[0])){
-                System.out.println("fisttopic");
-            }
-            if(message.getHeaders().get("mqtt_receivedTopic").equals(receiveTopics[1])){
-                System.out.println("secondtopic");
-            }
+        // System.out.println(message.getHeaders());
+        // System.out.println(message.getHeaders().get("mqtt_receivedTopic"));
+        
+        if(message.getHeaders().get("mqtt_receivedTopic").equals(receiveTopics[0])){
+            System.out.println("fist----topic");
+            // System.out.println(message.getPayload());
+            JSONObject result = JSONObject.parseObject(message.getPayload().toString());
+            System.out.println(result);
+
+        }
+        if(message.getHeaders().get("mqtt_receivedTopic").equals(receiveTopics[1])){
+            // System.out.println(message.getPayload());
+            JSONObject result = JSONObject.parseObject(message.getPayload().toString());
+            // System.out.println("second---topic");
+            System.out.println(result.getString("msg"));
+
+        }
             
        
         
